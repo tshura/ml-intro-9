8d16254d44aa3b65f5dba399deda6def851b4905
tshura <tasha19_98@mail.ru>
2022-05-09 23:16:09 +0300

    modif pipeline

--- forest_ml/src/forest_ml/pipeline.py
+++ forest_ml/src/forest_ml/pipeline.py
@@ -2,35 +2,39 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
+from sklearn.feature_selection import SelectFromModel
 
 
 def create_pipeline(
-    use_scaler: bool, s:str, m:str, max_iter: int, logreg_C: float, random_state: int, n:int, crit: str, m_depth: int
+    use_scaler: bool, s:str, m:str, max_iter: int, logreg_C: float, random_state: int, n:int, crit: str, m_depth: int, fs: bool
 ) -> Pipeline:
     pipeline_steps = []
+    if m == 'logreg':
+        model = LogisticRegression(
+                    random_state=random_state, max_iter=max_iter, C=logreg_C
+                )
+    elif m == 'rf':
+        model =  RandomForestClassifier(
+                    random_state=random_state, max_depth=m_depth, n_estimators=n, criterion = crit
+                )
     if use_scaler:
         if s == 'ss':
             pipeline_steps.append(("scaler", StandardScaler()))
         elif s == 'mm':
             pipeline_steps.append(("scaler", MinMaxScaler()))
-    if m == 'logreg':
-        pipeline_steps.append(
-            (
-                "classifier",
-                LogisticRegression(
-                    random_state=random_state, max_iter=max_iter, C=logreg_C
-                ),
-            )
-        )
-    elif m == 'rf':
-        pipeline_steps.append(
-            (
-                "classifier",
-                RandomForestClassifier(
-                    random_state=random_state, max_depth=m_depth, n_estimators=n, criterion = crit
-                ),
-            )
-        )
+    # if fs:
+    #     pipeline_steps.append(
+    #         (
+    #             "feature_selection",
+    #             SequentialFeatureSelector(),
+    #         )
+    #     )
     
+        pipeline_steps.append(
+            (
+                "classifier"
+                , model
+            )
+        )
+
     return Pipeline(steps=pipeline_steps)
