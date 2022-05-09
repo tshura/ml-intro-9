67e18a2815a7fd0a0485f7ee27972047be369524
tshura <tasha19_98@mail.ru>
2022-05-09 22:25:58 +0300

    add diff scaler and 2nd model

--- forest_ml/src/forest_ml/pipeline.py
+++ forest_ml/src/forest_ml/pipeline.py
@@ -1,20 +1,36 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
 
 
 def create_pipeline(
-    use_scaler: bool, max_iter: int, logreg_C: float, random_state: int
+    use_scaler: bool, s:str, m:str, max_iter: int, logreg_C: float, random_state: int, n:int, crit: str, m_depth: int
 ) -> Pipeline:
     pipeline_steps = []
     if use_scaler:
-        pipeline_steps.append(("scaler", StandardScaler()))
-    pipeline_steps.append(
-        (
-            "classifier",
-            LogisticRegression(
-                random_state=random_state, max_iter=max_iter, C=logreg_C
-            ),
+        if s == 'ss':
+            pipeline_steps.append(("scaler", StandardScaler()))
+        elif s == 'mm':
+            pipeline_steps.append(("scaler", MinMaxScaler()))
+    if m == 'logreg':
+        pipeline_steps.append(
+            (
+                "classifier",
+                LogisticRegression(
+                    random_state=random_state, max_iter=max_iter, C=logreg_C
+                ),
+            )
         )
-    )
+    elif m == 'rf':
+        pipeline_steps.append(
+            (
+                "classifier",
+                RandomForestClassifier(
+                    random_state=random_state, max_depth=m_depth, n_estimators=n, criterion = crit
+                ),
+            )
+        )
+    
     return Pipeline(steps=pipeline_steps)
