90101192fd85066150f248dde6cd432a08a95ac5
tshura <tasha19_98@mail.ru>
2022-05-09 16:57:32 +0300

    test split processing

--- forest_ml/src/forest_ml/data.py
+++ forest_ml/src/forest_ml/data.py
@@ -7,13 +7,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def get_dataset(
-    csv_path: Path, random_state: int, test_split_ratio: float
+    csv_path: Path, random_state: int,  test_split_ratio: float, use_tsr: bool
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     dataset = pd.read_csv(csv_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=test_split_ratio, random_state=random_state
-    )
-    return features_train, features_val, target_train, target_val
+    if use_tsr is not False:
+        features_train, features_val, target_train, target_val = train_test_split(
+            features, target, test_size=test_split_ratio, random_state=random_state
+        )
+        return features_train, features_val, target_train, target_val
+    else:
+        return features, target
---
+++ forest_ml/src/forest_ml/train.py
@@ -0,0 +1,106 @@
+from pathlib import Path
+from joblib import dump
+
+import click
+# import mlflow
+# import mlflow.sklearn
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_validate
+
+from .data import get_dataset
+from .pipeline import create_pipeline
+
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
+
+@click.option(
+    "--use-tsr",
+    default=False,
+    type=bool,
+    show_default=True,
+)
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
+)
+@click.option(
+    "--use-scaler",
+    default=True,
+    type=bool,
+    show_default=True,
+)
+@click.option(
+    "--max-iter",
+    default=500,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--logreg-c",
+    default=1.0,
+    type=float,
+    show_default=True,
+)
+def train(
+    dataset_path: Path,
+    save_model_path: Path,
+    random_state: int,
+    test_split_ratio: float,
+    use_scaler: bool,
+    use_tsr: bool,
+    max_iter: int,
+    logreg_c: float,
+) -> None:
+    if use_tsr is not False:
+        features_train, features_val, target_train, target_val = get_dataset(
+            dataset_path,
+            random_state,
+            test_split_ratio,
+            use_tsr
+        )
+    else:
+        features_train, target_train = get_dataset(
+            dataset_path,
+            random_state,
+            test_split_ratio,
+            use_tsr
+        )
+    # with mlflow.start_run():
+    pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
+    # print(pipeline)
+    print(features_train.shape, target_train.shape)
+    # cv_results = cross_validate(pipeline, features_train, target_train, cv=5)
+    # print(cv_results)
+    pipeline.fit(features_train, target_train)
+    
+    # accuracy = accuracy_score(target_val, pipeline.predict(features_val))
+    # mlflow.log_param("use_scaler", use_scaler)
+    # mlflow.log_param("max_iter", max_iter)
+    # mlflow.log_param("logreg_c", logreg_c)
+    # mlflow.log_metric("accuracy", accuracy)
+    # click.echo(f"Accuracy: {accuracy}.")
+    dump(pipeline, save_model_path)
+    click.echo(f"Model is saved to {save_model_path}.")
