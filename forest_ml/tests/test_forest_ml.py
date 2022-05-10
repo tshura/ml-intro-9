ff47c01163966327ced393398cb21c2298e04fc0
tshura <tasha19_98@mail.ru>
2022-05-10 23:33:37 +0300

    add criterion test

--- forest_ml/tests/test_forest_ml.py
+++ forest_ml/tests/test_forest_ml.py
@@ -1,5 +1,25 @@
-from forest_ml import __version__
+from click.testing import CliRunner
+import pytest
 
+from forest_ml.train import train
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture providing click runner."""
+    return CliRunner()
+
+def test_error_for_invalid_criterion(
+    runner: CliRunner
+) -> None:
+    """It fails when criterion is not in ['gini', 'entropy']"""
+    result = runner.invoke(
+        train,
+        [
+            "--criterion",
+            'foo',
+        ],
+    )
+    assert result.exit_code == 2
+    assert "criterion takes values 'gini' or 'entropy'" in result.output
+
+    
